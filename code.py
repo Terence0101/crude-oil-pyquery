f32c97d507bb0b055498c035bee7491adfb27a5e
Shuyi Fang <66406517+Terence0101@users.noreply.github.com>
2020-10-08 10:38:16 +0800

    Update code.py

--- code.py
+++ code.py
@@ -1,21 +1,21 @@
 import requests
 from pyquery import PyQuery as pq
-response = requests.get('https://www.macrotrends.net/1369/crude-oil-price-history-chart')
 
-infoset = []
+response = requests.get('https://www.macrotrends.net/1369/crude-oil-price-history-chart')
 doc = pq(response.text)
 doc.make_links_absolute(base_url=response.url)
 
+infoset = []
 for data in doc("#style-1 > table > tbody tr:nth-child(n)").items():
     infoDict = {}
     infoDict["Year"] = data("td:nth-child(1)").text()
     infoDict["Price"] = data("td:nth-child(2)").text()
     infoset.append(infoDict)
 
-a = int(input())
-b = int(input())
+beginYear = int(input('Start Year Before 2020: '))
+endYear = int(input('End Year After 1987: '))
 result = []
-for i in range (2020-a,2020-b+1):
+for i in range (2020-beginYear,2020-endYear+1):
     result += [infoset[i]]
     
 result.sort(key=lambda k: (k.get('Price', 0), k.get('Year', 0)), reverse=False)
